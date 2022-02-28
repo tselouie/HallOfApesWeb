2ec330a27863f40f957bd4908050f3e906edb8f3
Louie Tse <tselouie@gmail.com>
2022-02-28 16:05:27 -0500

    npm run build

---
+++ build/precache-manifest.1213617bc0828342911c19daf15c3711.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c0e24fa46d89cf7d32a4bff9aa7de19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fc0c394856a00d8dd64",
+    "url": "/static/css/main.cd233238.chunk.css"
+  },
+  {
+    "revision": "c559474800037452ea52",
+    "url": "/static/js/2.5577eab0.chunk.js"
+  },
+  {
+    "revision": "6fc0c394856a00d8dd64",
+    "url": "/static/js/main.edfe91fc.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
